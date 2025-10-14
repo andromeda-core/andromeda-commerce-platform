b2cb968c5c6f7030dab8f5ee0625f9098368f27c
ApexUnified <apexunified@gmail.com>
2025-10-14 23:25:46 +0500

    UPDATED

--- resources/js/Pages/Website/Home/index.jsx
+++ resources/js/Pages/Website/Home/index.jsx
@@ -666,13 +666,13 @@ export default function index({ google_map_api_key, search_history }) {
         window.addEventListener('wheel', handleWheel, { passive: false });
         container.addEventListener('touchstart', handleTouchStart, { passive: true });
         container.addEventListener('touchmove', handleTouchMove, { passive: true });
-        container.addEventListener('touchend', handleTouchEnd, { passive: true });
+        // container.addEventListener('touchend', handleTouchEnd, { passive: true });
 
         return () => {
             window.removeEventListener('wheel', handleWheel);
             container.removeEventListener('touchstart', handleTouchStart);
             container.removeEventListener('touchmove', handleTouchMove);
-            container.removeEventListener('touchend', handleTouchEnd);
+            // container.removeEventListener('touchend', handleTouchEnd);
             cancelAnimationFrame(momentumCheck.current);
         };
     }, [
