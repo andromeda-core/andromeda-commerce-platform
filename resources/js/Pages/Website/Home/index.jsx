21c6d460a8bc9b7a8d34b362acf3046c40e051a4
ApexUnified <apexunified@gmail.com>
2025-10-14 15:51:37 +0500

    UPDATED

--- resources/js/Pages/Website/Home/index.jsx
+++ resources/js/Pages/Website/Home/index.jsx
@@ -414,8 +414,8 @@ export default function index({ google_map_api_key, search_history }) {
         }
     }, [isMobilePostViewer, viewablePost]);
 
-    const scrollLock = useRef(false);
     const touchStartY = useRef(0);
+    const touchStartTime = useRef(0);
 
     // console.log(mobilePostContainerRef.current);
 
@@ -467,9 +467,11 @@ export default function index({ google_map_api_key, search_history }) {
             const handleTouchStart = (e) => {
                 touchStartY.current = e.touches[0].clientY;
                 touchStartTime.current = Date.now();
+                alert('MOBILE POST VIEWER SCROLL starts');
             };
 
             const handleTouchEnd = (e) => {
+                alert('MOBILE POST VIEWER SCROLL End');
                 const touchEndY = e.changedTouches[0].clientY;
                 const deltaY = touchStartY.current - touchEndY;
                 const elapsed = Date.now() - touchStartTime.current;
