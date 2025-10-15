72916d947553b12b294fafbbd55faaec4ac6e2b2
ApexUnified <apexunified@gmail.com>
2025-10-15 20:47:22 +0500

    inde xissue fixed

--- resources/js/Pages/Website/Home/index.jsx
+++ resources/js/Pages/Website/Home/index.jsx
@@ -514,7 +514,7 @@ export default function index({ google_map_api_key, search_history }) {
             if (newIndex !== selectedPostIndex && posts[newIndex]) {
                 setSelectedPostIndex(newIndex);
                 setViewablePost(posts[newIndex]);
-                setRelatedPosts(posts[nextIndex]?.related_posts);
+                setRelatedPosts(posts[newIndex]?.related_posts);
                 window.history.replaceState({}, '', generateURL(posts[newIndex]));
 
                 if (newIndex >= posts.length - 5 && nextPageUrl) {
@@ -538,6 +538,8 @@ export default function index({ google_map_api_key, search_history }) {
         nextPageUrl,
         isMobilePostGallery,
     ]);
+
+    console.log(relatedPosts);
 
     return (
         <MainLayout>
