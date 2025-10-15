01f060e0ae3a5f8a1ee9b40c425b433c848b5746
ApexUnified <apexunified@gmail.com>
2025-10-15 15:51:57 +0500

    UPDATED

--- resources/js/Pages/Website/Home/index.jsx
+++ resources/js/Pages/Website/Home/index.jsx
@@ -1098,6 +1098,7 @@ export default function index({ google_map_api_key, search_history }) {
                                             scrollSnapType: 'y mandatory',
                                             scrollSnapStop: 'always',
                                             WebkitOverflowScrolling: 'touch',
+                                            scrollBehavior: 'smooth',
                                         }}
                                         ref={mobilePostContainerRef}
                                         onFocus={() => mobilePostContainerRef.current?.focus()}
