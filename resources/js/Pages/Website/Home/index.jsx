1cc862b2e1d2ca424a3bb47cc968733bc781a4f0
ApexUnified <apexunified@gmail.com>
2025-10-16 03:33:58 +0500

    Skeleton Wraped with createPortal

--- resources/js/Pages/Website/Home/index.jsx
+++ resources/js/Pages/Website/Home/index.jsx
@@ -1853,7 +1853,8 @@ export default function index({ google_map_api_key, search_history }) {
                                                         ))}
 
                                                     {relatedPosts?.length > 0 &&
-                                                        isFetchingRelated && (
+                                                        isFetchingRelated &&
+                                                        createPortal(
                                                             <div className="relative flex h-full min-w-full flex-shrink-0 snap-start snap-always flex-col items-center justify-center bg-deepcharcoal text-white">
                                                                 <div className="absolute left-4 top-4 h-4 w-16 animate-pulse rounded bg-gray-700"></div>
                                                                 <div className="h-[70vh] w-[90%] animate-pulse rounded-2xl bg-gray-800/60"></div>
@@ -1862,7 +1863,8 @@ export default function index({ google_map_api_key, search_history }) {
                                                                     <div className="mx-auto h-4 w-1/2 animate-pulse rounded bg-gray-700/70"></div>
                                                                     <div className="mx-auto h-4 w-1/3 animate-pulse rounded bg-gray-700/60"></div>
                                                                 </div>
-                                                            </div>
+                                                            </div>,
+                                                            document.body,
                                                         )}
                                                 </div>
 
