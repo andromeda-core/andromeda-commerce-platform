36103671ddc02e940c7efea06dc93fa21d310213
ApexUnified <apexunified@gmail.com>
2025-10-19 19:45:57 +0500

    Updated

--- resources/js/Pages/Website/Home/index.jsx
+++ resources/js/Pages/Website/Home/index.jsx
@@ -553,6 +553,7 @@ export default function index({ google_map_api_key, search_history }) {
     const gestureLocked = useRef(null);
     const lastTriedRef = useRef(0);
 
+    const lastHorizontalIndexRef = useRef({});
     const horizontalCarouselRefs = useRef({});
 
     useEffect(() => {
