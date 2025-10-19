c0b015f4c4c06722e8dcac6c44ffcb2aba9519eb
ApexUnified <apexunified@gmail.com>
2025-10-20 01:00:33 +0500

    ADED Gesture Unloack After X axis to allow Y axis Scroll imedieatly

--- resources/js/Pages/Website/Home/index.jsx
+++ resources/js/Pages/Website/Home/index.jsx
@@ -895,6 +895,8 @@ export default function index({ google_map_api_key, search_history }) {
                             );
                             console.log('✅ Loop LEFT done');
                         }
+
+                        gestureLocked.current = null;
                         setTimeout(() => (isHorizontalLooping.current[slug] = false), 500);
                     });
                     return;
@@ -923,6 +925,7 @@ export default function index({ google_map_api_key, search_history }) {
                             `${route('home')}${generateURL(viewablePost)}`,
                         );
                         console.log('✅ Loop RIGHT done');
+                        gestureLocked.current = null;
                         setTimeout(() => (isHorizontalLooping.current[slug] = false), 500);
                     });
                     return;
