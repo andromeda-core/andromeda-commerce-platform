1105ef02dd5c455fd3c5bf93e05afe4aa3ef43dd
ApexUnified <apexunified@gmail.com>
2025-10-20 16:42:51 +0500

    Animation Stoped Of X axis contianer looping

--- resources/js/Pages/Website/Home/index.jsx
+++ resources/js/Pages/Website/Home/index.jsx
@@ -896,7 +896,7 @@ export default function index({ google_map_api_key, search_history }) {
                     horizontalContainer.style.pointerEvents = 'none';
 
                     const targetScroll = maxScroll;
-                    horizontalContainer.scrollTo({ left: targetScroll, behavior: 'smooth' });
+                    horizontalContainer.scrollTo({ left: targetScroll, behavior: 'auto' });
 
                     waitForSettle(targetScroll, () => {
                         const relatedPost = relatedPosts[relatedPosts.length - 1];
@@ -936,7 +936,7 @@ export default function index({ google_map_api_key, search_history }) {
                     horizontalContainer.style.touchAction = 'none';
                     horizontalContainer.style.pointerEvents = 'none';
 
-                    horizontalContainer.scrollTo({ left: 0, behavior: 'smooth' });
+                    horizontalContainer.scrollTo({ left: 0, behavior: 'auto' });
 
                     waitForSettle(0, () => {
                         setActiveViewerMap((p) => ({ ...p, [slug]: 'main' }));
