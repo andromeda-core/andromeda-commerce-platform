2ca9b3125ee6e34cf478fa313e31c423d93091a0
ApexUnified <apexunified@gmail.com>
2025-11-02 02:05:49 +0500

    Filter Spinner CSS FIXED

--- resources/js/Components/Spinner.jsx
+++ resources/js/Components/Spinner.jsx
@@ -5,7 +5,7 @@ export default function Spinner({ customSize }) {
         <div role="status">
             <svg
                 aria-hidden="true"
-                className={`${customSize ?? 'h-4 w-8'} animate-spin fill-indigo-600 text-gray-200 dark:text-gray-600`}
+                className={`${customSize ?? 'h-4 w-8'} animate-spin fill-indigo-600 text-gray-200 dark:text-white`}
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
