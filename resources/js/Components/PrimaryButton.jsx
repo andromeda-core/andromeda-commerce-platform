96a28ab2fb3ecca933e936325249799707e13796
ApexUnified <apexunified@gmail.com>
2025-11-03 03:09:32 +0500

    Priamry Button Spinner Color Changed

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -27,7 +27,7 @@ export default function PrimaryButton({
                         <div role="status">
                             <svg
                                 aria-hidden="true"
-                                className="h-4 w-8 animate-spin fill-indigo-600 text-gray-200 dark:text-gray-600"
+                                className="h-4 w-8 animate-spin fill-indigo-600 text-white"
                                 viewBox="0 0 100 101"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
