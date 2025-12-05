24ed361efb928f524944c328a6e29a26a0bd200f
ApexUnified <apexunified@gmail.com>
2025-12-05 17:21:00 +0500

    UPDATED

--- resources/js/Pages/Website/Home/MobileFeed.jsx
+++ resources/js/Pages/Website/Home/MobileFeed.jsx
@@ -1691,7 +1691,7 @@ const MobileFeed = ({
                                     }}
                                 >
                                     <div
-                                        className="flex w-full overflow-x-auto snap-x snap-mandatory"
+                                        className="flex w-full overflow-x-auto snap-x snap-mandatory scrollbar-none"
 
                                         ref={(el) => {
                                             horizontalRefs.current[index] = el;
