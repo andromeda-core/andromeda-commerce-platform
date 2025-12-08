19294b6daa70f09dc4d0a4d0fe7e01c05fc2b9c8
ApexUnified <apexunified@gmail.com>
2025-12-08 15:08:20 +0500

    Updates Some Layout CSS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,7 +26,10 @@
     <link rel="manifest" href="{{ route('pwa.manifest') }}">
     <meta name="theme-color" content="#f1f5f9">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
     <link rel="preconnect" href="{{ $aws_url }}" />
     <link rel="dns-prefetch" href="{{ $aws_url }}" />
