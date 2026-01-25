113db4c641da11b3c0a9cf1b1a1a09610e76a06d
ApexUnified <apexunified@gmail.com>
2026-01-25 10:17:31 +0500

    EXTRA DOT REMOVED FROM DATADELETION POLICY REVIEW TEXT

--- resources/js/Pages/Website/DataDeletionRequest/index.jsx
+++ resources/js/Pages/Website/DataDeletionRequest/index.jsx
@@ -185,7 +185,6 @@ const DataDeletion = () => {
                                                     >
                                                         {__('Please review our Privacy Policy.')}
                                                     </Link>
-                                                    .
                                                 </span>
                                             </label>
                                         </div>
