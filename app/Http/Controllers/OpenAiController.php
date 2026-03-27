1bf956af1443a833c4600b2afc6009055e424666
ApexUnified <apexunified@gmail.com>
2026-03-27 15:04:18 +0500

    VALIDATIONISSUE OF IMAGE THAT is beign sending from Samsung devices has been fixed

--- app/Http/Controllers/OpenAiController.php
+++ app/Http/Controllers/OpenAiController.php
@@ -8,16 +8,15 @@ use Illuminate\Http\Request;
 
 class OpenAiController extends Controller
 {
-
     public function aiDecode(Request $request, OpenAi $openAi): JsonResponse
     {
         $request->validate([
-            'image' => ['required', 'image', 'mimes:jpeg,jpg,png,webp', 'max:8192'],
+            'image' => ['required', 'file', 'max:10240'],
         ]);
 
-        $file      = $request->file('image');
+        $file = $request->file('image');
         $imageData = base64_encode(file_get_contents($file->getRealPath()));
-        $mimeType  = $file->getMimeType();
+        $mimeType = $file->getMimeType();
 
         $result = $openAi->getScannedBarcodeResults($imageData, $mimeType);
 
