8b9076aa47ef23b4d5d6951422671e7185d323cd
ApexUnified <apexunified@gmail.com>
2026-04-02 00:34:04 +0500

    PAckage Recording Delete Video Functionality Added

--- resources/js/Pages/Dashboard/Orders/show.jsx
+++ resources/js/Pages/Dashboard/Orders/show.jsx
@@ -2,7 +2,7 @@ import Card from '@/Components/Card';
 import LinkButton from '@/Components/LinkButton';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import BreadCrumb from '@/Components/BreadCrumb';
-import { Head, useForm, usePage } from '@inertiajs/react';
+import { Head, router, useForm, usePage } from '@inertiajs/react';
 import React, { useEffect, useState } from 'react';
 import PrimaryButton from '@/Components/PrimaryButton';
 import Swal from 'sweetalert2';
@@ -10,12 +10,17 @@ import Toast from '@/Components/Toast';
 import Placeholder from 'asset/assets/images/product/placeholder.jpg';
 import { useTranslation } from '@/Hooks/useTranslation';
 import PackageVerificationRecorder from '@/Components/PackageVerificationRecorder';
+import { useConfirm } from '@/Hooks/useConfirm';
+import Spinner from '@/Components/Spinner';
+import can from '@/Hooks/useCan';
 
 export default function show({ order }) {
     const { currency } = usePage().props;
     const [downloading, setDownloading] = useState(false);
     const { __ } = useTranslation();
     const [ValidationErrors, setValidationErrors] = useState({});
+
+    const { confirm, ConfirmDialog } = useConfirm();
 
     const getStatusColor = (status) => {
         const colors = {
@@ -69,7 +74,7 @@ export default function show({ order }) {
     const [videoIsntBeignUploadedYetOnAWS, setVideoIsntBeignUploadedYetOnAWS] = useState(false);
     const [verificationStatus, setVerificationStatus] = useState(null);
     const [verificationMessage, setVerificationMessage] = useState('');
-
+    const [deleteingVideo, setDeletingVideo] = useState(null);
 
     useEffect(() => {
         if (package_video?.package_video) {
@@ -92,6 +97,30 @@ export default function show({ order }) {
     }, [videoIsntBeignUploadedYetOnAWS]);
 
 
+
+
+
+    const deletePackageVideo = async (packageID) => {
+
+        const result = await confirm({
+            title: 'Confirm Delettion',
+            text: 'Are you sure you want to Delete This Package Video?',
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No',
+        });
+
+        if (result.isConfirmed) {
+            setDeletingVideo(packageID);
+            router.delete(route('dashboard.package-recordings.destroy', packageID), {
+                onFinish: () => setDeletingVideo(null),
+            });
+        }
+
+
+    }
+
     return (
         <>
             <AuthenticatedLayout>
@@ -104,6 +133,7 @@ export default function show({ order }) {
                     child={'View Order'}
                 />
 
+                <ConfirmDialog />
                 {Object.keys(ValidationErrors).length > 0 && (
                     <Toast flash={{ error: Object.values(ValidationErrors)[0] }} />
                 )}
@@ -481,6 +511,28 @@ export default function show({ order }) {
                                                             >
                                                                 <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>
                                                             </button>
+
+                                                            {can('Package Recordings Delete') && (
+                                                                <button
+                                                                    onClick={() => { deletePackageVideo(item.id) }}
+                                                                    className="flex items-center justify-center text-red-600 transition-colors rounded-full h-9 w-9 bg-red-50 hover:bg-red-100 dark:bg-zinc-900/80 dark:text-red-400"
+                                                                >
+
+
+                                                                    {deleteingVideo === item.id ? (
+                                                                        <>
+                                                                            <Spinner customSize={"size-3"} />
+                                                                        </>
+                                                                    ) : (
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                                                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                                        </svg>
+                                                                    )}
+
+
+
+                                                                </button>
+                                                            )}
                                                         </div>
                                                     </div>
                                                 ))
