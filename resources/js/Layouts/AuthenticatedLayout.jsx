3d276ffae90338ed7c2bb902163bd50bbd9bf760
ApexUnified <apexunified@gmail.com>
2026-04-02 01:08:57 +0500

    EVENT TESTING

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -78,6 +78,7 @@ export default function AuthenticatedLayout({ children }) {
         const channel = window.Echo.private(`user.${auth.user.id}`);
 
         channel.listen('.order-verification-success', (e) => {
+            console.log("Received event:", e);
             setRealtimeToast(e.message);
         });
 
