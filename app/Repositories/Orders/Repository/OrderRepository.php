502430f6f7685f1e47ba39e2d1eb078bea0d4b8a
ApexUnified <apexunified@gmail.com>
2026-04-12 16:50:22 -0700

    DCOKER REDIS COmpose FILE ADDED AND FILE  SYSTEM CRLF CHANGED TO LF

--- app/Repositories/Orders/Repository/OrderRepository.php
+++ app/Repositories/Orders/Repository/OrderRepository.php
@@ -958,7 +958,8 @@ class OrderRepository implements IOrderRepository
             // $this->ip_resolver->resolveCountryIDFromIP($request->ip()),
             // $shipping_address->country_id
             // );
-            if ($this->ip_resolver->resolveCountryIDFromIP($request->ip()) != $shipping_address->country_id) {
+
+            if (!app()->isLocal() && ($this->ip_resolver->resolveCountryIDFromIP($request->ip()) != $shipping_address->country_id)) {
                 throw new Exception($this->trans::get('For security reasons, the shipping address country must match your current detected location to place the order.'));
             }
 
